ce28c7865dd67b96c2cb6109dc797686c6bdba87
VISH01AL <g.vish1al@gmail.com>
2025-02-16 21:09:29 -0500

    started backend

---
+++ carRentalBookingSystemBackend/server.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const app = express();
+
+require("dotenv").config()
+
+app.listen(process.env.PORT||8080, ()=>{
+    console.log("Server is running on port",process.env.PORT||8080);
+});
