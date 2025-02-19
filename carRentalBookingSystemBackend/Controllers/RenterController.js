0e13346cbd5dcf0be078f4af8ce0b42f606244ac
VISH01AL <g.vish1al@gmail.com>
2025-02-18 19:59:56 -0500

    added backend images login and signup

--- carRentalBookingSystemBackend/Controllers/RenterController.js
+++ carRentalBookingSystemBackend/Controllers/RenterController.js
@@ -3,13 +3,26 @@ const {CarRental}=require("../models/index")
 const bcrypt = require("bcrypt")
 const jwt=require("jsonwebtoken")
 const validator = require("validator")
+const {S3Client,PutObjectCommand, GetObjectCommand }=require("@aws-sdk/client-s3")
+const {getSignedUrl }=require("@aws-sdk/s3-request-presigner")
+require("dotenv").config()
+const MAX_SIZE = 2 * 1024 * 1024;
 
-
+const s3 = new S3Client({
+    region: process.env.AWS_REGION, // e.g., "us-east-1"
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_ID,
+      secretAccessKey: process.env.AWS_ACCESS_SECRET_KEY,
+    },
+  });
+  
 const signup=async(req,res)=>{
 try{
-    const {firstName,lastName,email,password,phone,address,profilePicture}=req.body;
-    console.log(firstName,lastName,email,password)
-    if(!firstName || !lastName || !email || !profilePicture || !address || !password) {
+    const {firstName,lastName,email,password,phone,address}=req.body;
+    if(req.file.size>MAX_SIZE){
+        return res.status(400).json({ message: "File size exceeds 2MB limit!" });
+    }
+    if(!firstName || !lastName || !email || !req.file || !address || !password) {
         return res.status(400).send({message:"All fields are required"});
     }
     if(!validator.isEmail(email)) {
@@ -19,9 +32,19 @@ try{
         return res.status(400).send({message:"Password must be at least 8 characters long, include a number, an uppercase letter, and a special character."});
     }
     const existingUser=await CarRental.findOne({where:{email}});
-    if(existingUser) return res.status(400).send({message:"Renter already exists"});
+    if(existingUser) return res.status(400).send({message:"Renter email already exists"});
 
     const hashpassword=await HashPassword(password);
+    const uuid = crypto.randomUUID().replace(/-/g, "");
+    console.log(process.env.AWS_ACCESS_ID,process.env.AWS_ACCESS_SECRET_KEY)
+    const uploadParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: uuid+".jpg", 
+        Body: req.file.buffer,
+      };
+    const command = new PutObjectCommand(uploadParams);
+    const response = await s3.send(command);
+
     const carUser=await CarRental.create({
         firstName,
         lastName,
@@ -29,15 +52,23 @@ try{
         password:hashpassword,
         phone,
         address,
-        profilePicture
+        profilePicture:uuid+".jpg"
     })
+    const command2 = new GetObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: uuid+".jpg",
+      });
+      const url = await getSignedUrl(s3, command2, { expiresIn: 3600*24 }); // Expires in 1 hour (3600 sec)
+
+  
     res.status(200).send({
         email: carUser.email,
         token: token(carUser.id),
-        profilePicture: carUser.profilePicture
+        profilePicture: url
     })
 }
 catch(err){
+    console.log(err)
     res.status(500).send({message:"signup request failed"});
 }
 }
@@ -53,10 +84,17 @@ const login=async(req,res)=>{
         if(!carUser) return res.status(404).send({message:"Renter not found"});
         const isMatch=await bcrypt.compare(password,carUser.password);
         if(!isMatch) return res.status(400).send({message:"Invalid credentials"});
+        const command2 = new GetObjectCommand({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: carUser.profilePicture,
+          });
+          const url = await getSignedUrl(s3, command2, { expiresIn: 3600*24 }); // Expires in 1 hour (3600 sec)
+    
+      
         res.status(200).send({
             email: carUser.email,
             token: token(carUser.id),
-            profilePicture: carUser.profilePicture
+            profilePicture: url
         })
     }
     catch{
--- carRentalBookingSystemBackend/Routers/RenterRoutes.js
+++ carRentalBookingSystemBackend/Routers/RenterRoutes.js
@@ -1,8 +1,11 @@
 const express=require("express")
 const renterRouter=express.Router();
 const {signup,login}=require("../Controllers/RenterController")
+const multer=require("multer")
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-renterRouter.post("/signup",signup)
+renterRouter.post("/signup",upload.single("profilePicture"),signup)
 renterRouter.post("/login",login)
 
 module.exports=renterRouter;
